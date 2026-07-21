1c410cf1492877ebd9dedff65fdfe3297584ddef
Jake Sherwood <72046993+sherwoodjm@users.noreply.github.com>
2026-07-20 20:48:13 -0700

    Delete assets directory

--- assets/tiled/desert_tomb_v2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="desert_tomb_v2" tilewidth="16" tileheight="16" tilecount="132" columns="12">
- <image source="../desert_tomb_v2.png" width="192" height="176"/>
-</tileset>
